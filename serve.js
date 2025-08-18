ea5f3a33e1a3b3d33c5c2ecbbf8d16392c1ace7e
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-19 00:37:58 +0900

    自分用にサーバー点けるスクリプト追加

---
+++ serve.js
@@ -0,0 +1,3 @@
+// 伝説的カスコード
+var r=require,{fromEntries:e}=Object,p=Response,{redirect:d}=p,s=`\x2f`
+export default{routes:{...e([...new(r("\x62u\x6e")["\x47l\x6fb"])("\x2a.\x68\x74\x6dl")["\x73c\x61n\x53y\x6ec"]("\x2e")].map(e=>[s+e,r("\x2e/"+e)])),"\x2f":d(`${s}\x68o\x6de.\x68t\x6dl`)},port:0xbba,fetch:c=>d(s)}
